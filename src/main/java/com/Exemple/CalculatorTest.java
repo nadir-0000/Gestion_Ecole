c24b3d3b7751579f07a9d4937ce4e15d858519fb
Nadir El Mesmoudi <127327456+nadir-0000@users.noreply.github.com>
2024-04-16 09:57:30 +0100

    Update CalculatorTest.java

--- src/main/java/com/Exemple/CalculatorTest.java
+++ src/main/java/com/Exemple/CalculatorTest.java
@@ -1,16 +1,1 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-public class CalculatorTest {
 
-    @Test
-    public void testAddition() {
-        // Arrange
-        Calculator calculator = new Calculator();
-
-        // Act
-        int result = calculator.add(7, 5);
-
-        // Assert
-        assertEquals(12, result);
-    }
-}
